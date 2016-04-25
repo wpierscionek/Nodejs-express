9de6b2ebb55fbee127688e779b15be74bf931737
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-04-25 18:19:34 -0400

    4/25/2016

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require('express'); // requires express package
+var bodyParser = require('body-parser'); // requires body-parser package
+var path = require('path'); // requires path package
+
+var app = express(); //creating express server
+var PORT = process.env.PORT || 80; //setting up port which we use for app
+
+//below code comes from body-parser package that makes it easy for our server to interpret data sent to it
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
